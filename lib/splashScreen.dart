0e7cca565ccd15a59363f30f00034a4a67f74df7
dallahdev123 <toureabdoullah2552@gmail.com>
2021-07-09 01:54:25 +0000

    setUp plugins and home page

---
+++ lib/splashScreen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:splashscreen/splashscreen.dart';
+import 'home.dart';
+
+class MySplah extends StatefulWidget {
+  const MySplah({Key? key}) : super(key: key);
+
+  @override
+  _MySplahState createState() => _MySplahState();
+}
+
+class _MySplahState extends State<MySplah> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 2,
+      navigateAfterSeconds: Home(),
+      title: Text(
+        'Cat and Dog Classifier',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          color: Color(0x00FFFF),
+        ),
+      ),
+      image: Image.asset('assets/cat_dog_icon.png'),
+      backgroundColor: Colors.blueAccent,
+      photoSize: 60,
+      loaderColor: Color(0x004242),
+    );
+  }
+}
