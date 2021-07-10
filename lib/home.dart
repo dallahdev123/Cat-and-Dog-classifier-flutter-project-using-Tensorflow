ba3b03ba8dadbc82410ba702e9ec261c2712515b
dallahdev123 <toureabdoullah2552@gmail.com>
2021-07-10 04:23:18 +0000

    Setting model's Tensorflow-Lite and image_picker library

--- lib/home.dart
+++ lib/home.dart
@@ -1,14 +1,75 @@
+//import 'dart:html';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:tflite/tflite.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
-
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
   bool _loading = true;
+  late File _image;
+  late List _output;
+  final picker = ImagePicker();
+
+  @override
+  void initState() {
+    super.initState();
+    loadModel().then((value) {
+      setState(() {});
+    });
+  }
+
+  detectImage(File image) async {
+    var output = await Tflite.runModelOnImage(
+        path: image.path,
+        numResults: 2,
+        threshold: 0.6,
+        imageMean: 127.5,
+        imageStd: 127.5);
+    setState(() {
+      _output = output as List<dynamic>;
+      _loading = false;
+    });
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+        model: 'assete/model_unquant.tflite', labels: 'assets/labels.txt');
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  pickImage() async {
+    var image = await picker.getImage(source: ImageSource.camera);
+    if (image == null) return null;
+
+    setState(() {
+      _image = File(image.path);
+    });
+    detectImage(_image);
+  }
+
+  pickGalleryImage() async {
+    var image = await picker.getImage(source: ImageSource.gallery);
+    if (image == null) return null;
+
+    setState(() {
+      _image = File(image.path);
+    });
+    detectImage(_image);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,7 +91,7 @@ class _HomeState extends State<Home> {
           padding: EdgeInsets.symmetric(horizontal: 24),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
+            children: <Widget>[
               SizedBox(height: 8),
               Text('Cat and Dog recognition App',
                   style: TextStyle(
@@ -43,20 +104,44 @@ class _HomeState extends State<Home> {
                     ? Container(
                         width: 250,
                         child: Column(
-                          children: [
+                          children: <Widget>[
                             Image.asset('assets/cat_dog_icon.png'),
                             SizedBox(height: 40),
                           ],
                         ),
                       )
-                    : Container(),
+                    : Container(
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              height: 250,
+                              child: Image.file(_image),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            _output != null
+                                ? Text(
+                                    '${_output[0]['label']}',
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 15),
+                                  )
+                                : Container(),
+                            SizedBox(
+                              height: 10,
+                            ),
+                          ],
+                        ),
+                      ),
               ),
               Container(
                   width: MediaQuery.of(context).size.width,
                   child: Column(
-                    children: [
+                    children: <Widget>[
                       GestureDetector(
-                        onTap: () {},
+                        onTap: () {
+                          pickImage();
+                        },
                         child: Container(
                           width: MediaQuery.of(context).size.width - 250,
                           alignment: Alignment.center,
@@ -74,7 +159,9 @@ class _HomeState extends State<Home> {
                       ),
                       SizedBox(height: 5),
                       GestureDetector(
-                        onTap: () {},
+                        onTap: () {
+                          pickGalleryImage();
+                        },
                         child: Container(
                           width: MediaQuery.of(context).size.width - 250,
                           alignment: Alignment.center,
